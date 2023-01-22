debd738ed74939924e59041817e96234b7618c99
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-01-22 15:24:01 -0300

    feat: Adjust count state initialization and linting format

--- src/components/TS-hooks-counter.tsx
+++ src/components/TS-hooks-counter.tsx
@@ -1,44 +1,49 @@
 import React, {useState, useEffect, useRef} from "react";
 import "./styles.css";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const [stop, setSTop] = useState(false);
-  const [intervalId, setIntervalId] = useState();
+/**
+ * Author: Felipe Oliveira
+ * Description: Example of a React components using hooks and Typescript. In this
+ * case we don't have explicit type due to Typescript type inference.
+ */
+export default function TSHooksCounter() {
+    const [count, setCount] = useState(0);
+    const [stop, setSTop] = useState(false);
+    const [intervalId, setIntervalId] = useState(0);
 
-  useEffect(() => {
-    const id = returnIntervalId();
-    return () => clearInterval(id);
-  }, [stop]);
+    useEffect(() => {
+        const id = returnIntervalId();
+        return () => clearInterval(id);
+    }, [stop]);
 
-  const returnIntervalId = () => {
-    if (!stop) {
-      const id = setInterval(() => {
-        setCount((count) => count + 1);
-      }, 1000);
-      setIntervalId(id);
-      return id;
-    }
-  };
+    const returnIntervalId = () => {
+        if (!stop) {
+            const id = setInterval(() => {
+                setCount((count) => count + 1);
+            }, 1000);
+            setIntervalId(id);
+            return id;
+        }
+    };
 
-  const stopCount = () => {
-    clearInterval(intervalId);
-    setSTop(true);
-    setCount(0);
-  };
+    const stopCount = () => {
+        clearInterval(intervalId);
+        setSTop(true);
+        setCount(0);
+    };
 
-  const startCount = () => {
-    setSTop(false);
-    returnIntervalId();
-  };
+    const startCount = () => {
+        setSTop(false);
+        returnIntervalId();
+    };
 
-  return (
-    <div className="App">
-      <h1>{count}</h1>
-      <button onClick={stopCount}>Stop Count</button>
-      <br></br>
-      <br></br>
-      <button onClick={startCount}>Start Count</button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={stopCount}>Stop Count</button>
+            <br></br>
+            <br></br>
+            <button onClick={startCount}>Start Count</button>
+        </div>
+    );
 }
